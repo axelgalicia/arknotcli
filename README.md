ea34219f3d2987932dcb17b75c72e5b7cb554b1f
Axel Galicia <axelgalicia@gmail.com>
2019-04-28 21:13:46 -0400

    Finish CLI sample

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sample-cli
+
+A simple CLI Template. 
+
+This can be used as a template to create simple CLI interfaces using NodeJS.
+
+For this sample it will help you to create a file asking you for the filename and extension.
+
+```
+$ npm install
+```
+
+```
+$ arknotcli
+```
+
+@Author: Axel Galicia
---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+// Based on: https://codeburst.io/building-a-node-js-interactive-cli-3cb80ed76c86
+// https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e
+
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const shell = require('shelljs');
+
+const run = async () => {
+    // show script introduction
+    init();
+    // ask questions
+    const answers = await askQuestions();
+    const { FILENAME, EXTENSION } = answers;
+    // create the file
+    const filePath = createFile(FILENAME, EXTENSION);
+    // show success message
+    success(filePath);
+}
+
+// Script Introduction
+const init = () => {
+    console.log(
+        chalk.green(
+            figlet.textSync("Arknot   CLI", {
+                font: "small",
+                horizontalLayout: 'default',
+                verticalLayout: 'default'
+            })
+        )
+    );
+}
+
+// Questions to prompt
+const askQuestions = () => {
+    const questions = [
+        {
+            name: "FILENAME",
+            type: "input",
+            message: "What is the name of the file without extension?"
+        },
+        {
+            type: "list",
+            name: "EXTENSION",
+            message: "What is the file extension?",
+            choices: [".ts", ".js", ".html", ".css"],
+            filter: function (val) {
+                return val.split(".")[1];
+            }
+        }
+    ];
+    return inquirer.prompt(questions);
+};
+
+// Creates the file
+const createFile = (filename, extension) => {
+    const filePath = `${process.cwd()}/${filename}.${extension}`
+    shell.touch(filePath);
+    return filePath;
+};
+
+const success = (filepath) => {
+    console.log(
+        chalk.white.bgGreen.bold(`Done! File created at ${filepath}`)
+    );
+};
+
+run();
+
